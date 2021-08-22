58307d510530eee175fb625d5434f5f080c089e4
Raviikumar001 <samcollins9899@gmail.com>
2021-08-22 22:20:57 +0530

    added minion appi

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hi");
+const inputTextArea = document.querySelector("#input-text-area");
+const outputDiv = document.querySelector("#output-div");
+const translateButton = document.querySelector("#translate-btn");
+const miniApi = "https://api.funtranslations.com/translate/minion.json";
+
+const translateQuery = (text) => {
+  return miniApi + "?text=" + text;
+};
+const errorHandler = (err) => {
+  console.log(err);
+  alert("Something went wrong, Please try again later!");
+};
+
+const transelateText = () => {
+  const textToTranslate = inputTextArea.value;
+  fetch(translateQuery(textToTranslate))
+    .then((res) => res.json())
+    .then((json) => {
+      const tranlatedText = json.contents.translated;
+      outputDiv.innerText = tranlatedText;
+    })
+    .catch(errorHandler);
+};
+
+translateButton.addEventListener("click", transelateText);
